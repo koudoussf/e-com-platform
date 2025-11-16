40eedfd7f85b7fafc4098ec4c2e836acc78e4ef3
koudoussf <koudoussfataye08@gmail.com>
2025-11-16 20:50:06 +0100

    app push with all functionnalities except invoices pdf and internationalization

---
+++ payment/webhooks.py
@@ -0,0 +1,51 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+from shop.models import Product
+from shop.recommender import Recommender
+
+# from .tasks import payment_completed
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event.type == "checkout.session.completed":
+        session = event.data.object
+        if session.mode == "payment" and session.payment_status == "paid":
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+            # mark order as paid
+            order.paid = True
+            # store Stripe payment ID
+            order.stripe_id = session.payment_intent
+            order.save()
+            # launch asynchronous task
+
+            # save items bought for product recommendation
+            products_ids = order.items.values_list("product_id")
+            products = Product.objects.get(id__in=products_ids)
+            r = Recommender()
+            r.products_bought(products)
+
+            # payment_completed.delay(order.id)
+
+    return HttpResponse(status=200)
